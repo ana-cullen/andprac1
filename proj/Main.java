1ef0241f825ebed8de9c986bea3cca265a6490bf
ana c <anacullen466@gmail.com>
2021-11-15 16:13:30 +0100

    inout reader done

--- proj/Main.java
+++ proj/Main.java
@@ -20,12 +20,24 @@ public class Main {
     }
 
     public static void takeInput(int w, List <Coordinate> cl, List <Disk> dl) throws FileNotFoundException{
-        File myObj = new File("test.txt");
-        Scanner myReader = new Scanner(myObj);
+
+        Scanner myReader = new Scanner(System.in);
         String fstLn = myReader.nextLine();
         String[] splitted = fstLn.split(" ");
-        int n;
+        int n, m;
         n = Integer.parseInt(splitted[0]);
+        m = Integer.parseInt(splitted[1]);
+        w = Integer.parseInt(splitted[2]);
+        for(int i = 0; i < n; i++){
+            String coords = myReader.nextLine();
+            String[] split = coords.split(" ");
+            cl.add(new Coordinate(Integer.parseInt(split[0]), Integer.parseInt(split[0])));
+        }
+        for(int i = 0; i < m; i++){
+            String disk = myReader.nextLine();
+            String[] split = disk.split(" ");
+            dl.add(new Disk(Integer.parseInt(split[0]), Integer.parseInt(split[0])));
+        }
         System.out.println(n);
         myReader.close();
     }
