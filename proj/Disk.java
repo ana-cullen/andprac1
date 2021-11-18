be64b9591790d247bb16fa3759e66745bbb57d71
Marthijn van den Nieuwenhuizen <marthijn_vdn@hotmail.com>
2021-11-18 16:31:28 +0100

    graph created gl with the conflicts

--- proj/Disk.java
+++ proj/Disk.java
@@ -1,10 +1,7 @@
 package proj;
 
-<<<<<<< HEAD
-public class Disk implements Comparable<Disk>{
-=======
 public class Disk implements Comparable<Disk> {
->>>>>>> origin/main
+
 
     public int radius;
     public int cost;
@@ -12,17 +9,6 @@ public class Disk implements Comparable<Disk> {
     public Disk(int r, int c){
         this.radius = r;
         this.cost = c;
-    }
-
-    @Override
-    public int compareTo(Disk d){
-        if(this.radius < d.radius){
-            return -1;
-        }
-        if(this.radius == d.radius){
-            return 0;
-        }
-        return 1;
     }
 
     @Override
